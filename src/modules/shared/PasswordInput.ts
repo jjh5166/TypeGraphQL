fb015ec3f8f529ce94a9030a4a99be5b69f68bfd
John <jjh5166@gmail.com>
2020-06-08 21:39:19 -0400

    logout

--- src/modules/shared/PasswordInput.ts
+++ src/modules/shared/PasswordInput.ts
@@ -1,12 +1,14 @@
 import { Min } from "class-validator";
-import { Field, InputType } from 'type-graphql';
+import { Field, InputType, ClassType } from 'type-graphql';
 
-//input to share with all passport fields to ensure validation
+//input to share with all password fields to ensure validation
+export const PasswordMixin = <T extends ClassType>(BaseClass: T) => {
+  @InputType({ isAbstract: true }) //isAbstract required to prevent error
+  class PasswordInput extends BaseClass{
 
-@InputType()
-export class PasswordInput {
-
-  @Field()
+    @Field()
     @Min(5)
-  password: string;
+    password: string;
+  }
+  return PasswordInput
 }
