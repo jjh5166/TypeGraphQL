41b7eaf0515000630edd204e8696115ce2c5fe2d
John <jjh5166@gmail.com>
2020-06-24 13:30:58 -0400

    fix typo

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -1,13 +1,13 @@
-import { MiddlewareFn } from 'type-graphql';
-import { MyContext } from 'src/types/MyContext';
+import { MiddlewareFn } from "type-graphql";
+import { MyContext } from "src/types/MyContext";
 // custom auth middleware, alternative to Authorization feature
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
   if (!context.req.session!.userId) {
-    throw new Error("not authenitcated")
+    throw new Error("not authenticated");
     // Middleware can also break the middleware stack by not calling the next function.
     // This way, the result returned from the middleware will be used instead of calling
     // the resolver and returning it's result.
   }
 
-  return next(); 
+  return next();
 };
